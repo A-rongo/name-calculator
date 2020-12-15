f767a6668e8d31251b30574ce0ee2261d7ea295d
awadh <awadhhassan566@gmail.com>
2020-12-15 08:04:20 +0300

    addition of arithmetic functions

--- js/calc.js
+++ js/calc.js
@@ -1,24 +1,41 @@
-function submit() {
-    var birth = document.getElementById("birthID").value;
+var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"] 
+
+var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+
+function AkanNames() {
+    var century = document.getElementById("century").value;
     
-    var gender = document.getElementById("genderID").value
+    // alert(century)
+    var year = parseInt(document.getElementById("year").value);
+    // alert(year)
+    var mm = parseInt(document.getElementById("month").value);
+    alert(mm)
+    var day = document.getElementById("day").value;
+    alert(day)
+    var gender = document.getElementById("gender").value;
+    alert(gender)
 
-    if (gender=== male){ Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi,Kwame 
-    };
-    else( gender=== female) { Akosua,Adwoa,Abenaa,Akua,Yaa,Afua,Ama
-    };
-    var cC = parseInt(birth.substr(0, 2))
-    console.log(cC)
+    var date = parseInt(((century / 4) - 2 * century - 1) + ((5 * year/ 4)) + ((26 * (mm + 1) / 10)) + day) % 7
 
-    var yY = parseInt(birth.substr(2, 5))
-    console.log(yY)
-   var mM = parseInt(birth.substr(5,8))
-    console.log(mM)
-
-    var dD = parseInt(birth.substr(8,11))
-    console.log(dD)
-
-   
-    // var mod = function ( % )
-    var Dayoftheweek = ( ( (cC/4) -2*cC-1) + ((5*yY/4) ) + ((26*(mM+1)/10)) + dD )%7
+    if (year <= 0 || year == "" || year > 2020) {
+        alert("enter valid year")
+    }
+    if (century <=1 || century == 0){
+        alert("enter century" )
+    }
+    if (day <=31  || day ==0){
+        alert("enter day")
     
+   } 
+      if (month<=12  || month ==0){
+        alert("enter day")
+    }
+
+    if (gender === "male") {
+        alert("your Akan male name is" + maleName[date]);
+        
+    }
+    if (gender === "female") {
+        alert("your Akan male name is" + femaleName[date]);
+    };
